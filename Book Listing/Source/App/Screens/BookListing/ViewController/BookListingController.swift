a64d057cd27471d4f04d63a7eddef904d1e85b66
Onyekachi Ezeoke <ezeokeonyekachi@gmail.com>
2020-04-02 02:49:48 +0100

    configure the view controller to use the view model and bind to its delegate methods

--- Book Listing/Source/App/Screens/BookListing/ViewController/BookListingController.swift
+++ Book Listing/Source/App/Screens/BookListing/ViewController/BookListingController.swift
@@ -12,7 +12,36 @@ class BookListingController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+        viewModel = BookListingViewModel()
+        viewModel.delegate = self
+        viewModel.fetchBookList()
+    }
+    
+    var viewModel: BookListingViewModel!
+    var nextPageToken: String!
+    var bookListingCellViewModel: [BookListingCellViewModel] = []
+    var totalCount = 0
+    var headerTitle: String?
+}
+
+private extension BookListingController {
+    func setupViews() {
         view.backgroundColor = .orange
+    }
+}
+
+extension BookListingController: BookListingViewModelDelegate {
+    func didFetchBookList(_ nextPageToken: String, title: String, totalCount: Int, viewModel: [BookListingCellViewModel]) {
+        
+        self.bookListingCellViewModel.append(contentsOf: viewModel)
+        self.headerTitle = title
+        self.nextPageToken = nextPageToken
+        self.totalCount = totalCount
+    }
+    
+    func bookListFetchFailed(with error: String) {
+        
     }
 }
 
